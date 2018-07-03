97a091c8610beb85bb90021bde573330acab7085
PengFei <626138959@qq.com>
2018-07-03 22:28:26 +0800

    http跨域处理

--- seller-manage/js/loulan/service/base-service.js
+++ seller-manage/js/loulan/service/base-service.js
@@ -1,35 +1,38 @@
 
 app.factory("baseService", ["$http", function ($http) {
 
+    // 跨域认证请求头
+    let headers = { crossDomain:true };
+
     // 返回一个构造器，用于创造公共service
-    return function (urlPrefix) {
+    return function(urlPrefix) {
 
         // 分页查询
-        this.findPage = function (page, size, entity) {
-            var params = { page: page, size: size };
-            return $http.post(`${urlPrefix}/search.do`, entity, { params });
+        this.findPage = function(page, size, entity) {
+            var params = {page: page, size: size};
+            return $http.post(`${urlPrefix}/search.do`, entity, {params, headers, withCredentials: true});
         };
-       
+
         // 查询
-        this.findOne = function (id) {
-            let params = { id: id };
-            return $http.get(`${urlPrefix}/findOne.do`, { params });
+        this.findOne = function(id) {
+            let params = {id: id};
+            return $http.get(`${urlPrefix}/findOne.do`, {params, headers, withCredentials: true});
         };
 
         // 新增
-        this.add = function (entity) {
-            return $http.post(`${urlPrefix}/add.do`, entity);
+        this.add = function(entity) {
+            return $http.post(`${urlPrefix}/add.do`, entity, {headers, withCredentials: true});
         };
 
         // 更新
-        this.update = function (entity) {
-            return $http.post(`${urlPrefix}/update.do`, entity);
+        this.update = function(entity) {
+            return $http.post(`${urlPrefix}/update.do`, entity, {headers, withCredentials: true});
         };
 
         // 批量删除
-        this.delIds = function (ids) {
-            let params = { ids: ids.toString() };
-            return $http.get(`${urlPrefix}/deleteByIds.do`, { params });
+        this.delIds = function(ids) {
+            let params = {ids: ids.toString()};
+            return $http.get(`${urlPrefix}/deleteByIds.do`, {params, headers, withCredentials: true});
         };
 
     };
